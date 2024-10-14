8c4546bc3daa993d08a5967c4704440985aaf63c
amgx34 <xdichiara@outlook.com>
2024-10-13 20:02:17 -0400

    Initial commit

---
+++ src/CrapsGame.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.random.
+public class Craps {
+    public static void main(String[] args) {
+     Random dice = new Random();
+
+     int dice1 = rnd.nextInt(6) + 1;
+     int dice2 = rnd.nextInt(6) + 1;
+     int crapsRoll = dice1 + dice2;
+     
+
+
+
+
+
+    }
+}
